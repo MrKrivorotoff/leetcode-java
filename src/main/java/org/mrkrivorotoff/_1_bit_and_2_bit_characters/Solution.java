9f2626c12b9ac324b6333fc3db454d097fd9caf1
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-20 22:03:41 +0000

    [717. 1-bit and 2-bit Characters] Improve Solution

--- src/main/java/org/mrkrivorotoff/_1_bit_and_2_bit_characters/Solution.java
+++ src/main/java/org/mrkrivorotoff/_1_bit_and_2_bit_characters/Solution.java
@@ -2,12 +2,10 @@ package org.mrkrivorotoff._1_bit_and_2_bit_characters;
 
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        var numberOfBits = -1;
+        var i = 0;
         final var bitsLength = bits.length;
-        for (var i = 0; i < bitsLength;) {
-            numberOfBits = (bits[i] == 1) ? 2 : 1;
-            i += numberOfBits;
-        }
-        return numberOfBits == 1;
+        while (i < bitsLength - 1)
+            i += bits[i] + 1;
+        return i == bitsLength - 1;
     }
 }
