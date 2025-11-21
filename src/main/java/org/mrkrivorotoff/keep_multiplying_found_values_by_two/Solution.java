d2c7bf0724e1097b41996931d2b1abff4ce31a39
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-21 15:36:30 +0000

    [2154. Keep Multiplying Found Values by Two] Add alternative implementation

--- src/main/java/org/mrkrivorotoff/keep_multiplying_found_values_by_two/Solution.java
+++ src/main/java/org/mrkrivorotoff/keep_multiplying_found_values_by_two/Solution.java
@@ -1,14 +1,27 @@
 package org.mrkrivorotoff.keep_multiplying_found_values_by_two;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 class Solution {
     public int findFinalValue(int[] nums, int original) {
+        return findFinalValueUsingHashSet(nums, original);
+    }
+
+    private static int findFinalValueUsingHashSet(final int[] nums, int original) {
         final var setOfNums = new HashSet<Integer>();
         for (var num : nums)
             setOfNums.add(num);
         while (setOfNums.contains(original))
             original <<= 1;
+        return original;
+    }
+
+    private static int findFinalValueUsingSorting(final int[] nums, int original) {
+        Arrays.sort(nums);
+        for (var num : nums)
+            if (original == num)
+                original <<= 1;
         return original;
     }
 }
