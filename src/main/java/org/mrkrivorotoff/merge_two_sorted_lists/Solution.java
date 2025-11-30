b46634ec5ed3ca43b947c4b52abca45cf6d8b6e9
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-30 15:06:11 +0000

    [21. Merge Two Sorted Lists] Improve Solution

--- src/main/java/org/mrkrivorotoff/merge_two_sorted_lists/Solution.java
+++ src/main/java/org/mrkrivorotoff/merge_two_sorted_lists/Solution.java
@@ -7,48 +7,37 @@ class Solution {
         return mergeTwoListsToNewList(list1, list2);
     }
 
-    private static ListNode mergeTwoListsByChangingNodes(ListNode list1, ListNode list2) {
+    private static ListNode mergeTwoListsByChangingNodes(final ListNode list1, final ListNode list2) {
         final var rootNode = getCandidateToAppend(list1, list2);
         if (rootNode == null) return null;
 
         var candidateToPostpone = (rootNode == list1) ? list2 : list1;
-        var currentNode = rootNode;
-        while (true) {
+        for (var currentNode = rootNode; ; currentNode = currentNode.next) {
             final var candidateToAppend = getCandidateToAppend(currentNode.next, candidateToPostpone);
             if (candidateToAppend == null)
                 break;
             candidateToPostpone = (candidateToAppend == currentNode.next) ? candidateToPostpone : currentNode.next;
             currentNode.next = candidateToAppend;
-            currentNode = currentNode.next;
         }
-
         return rootNode;
     }
 
     private static ListNode mergeTwoListsToNewList(ListNode list1, ListNode list2) {
-        final var rootNode = getCandidateToAppend(list1, list2);
-        if (rootNode == null) return null;
-        final var newList = new ListNode(rootNode.val);
-
-        var node1 = rootNode.next;
-        var node2 = (rootNode == list1) ? list2 : list1;
-        var currentNode = newList;
-        while (true) {
-            final var candidate = getCandidateToAppend(node1, node2);
+        final var headNode = new ListNode();
+        for (var currentNode = headNode; ; currentNode = currentNode.next) {
+            final var candidate = getCandidateToAppend(list1, list2);
             if (candidate == null)
                 break;
-            if (candidate == node1)
-                node1 = node1.next;
+            if (candidate == list1)
+                list1 = list1.next;
             else
-                node2 = node2.next;
+                list2 = list2.next;
             currentNode.next = new ListNode(candidate.val);
-            currentNode = currentNode.next;
         }
-
-        return newList;
+        return headNode.next;
     }
 
-    private static ListNode getCandidateToAppend(ListNode node1, ListNode node2) {
+    private static ListNode getCandidateToAppend(final ListNode node1, final ListNode node2) {
         if (node1 == null) return node2;
         if (node2 == null) return node1;
         return node1.val < node2.val ? node1 : node2;
