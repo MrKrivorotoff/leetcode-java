b3422816db00a7655618df3cf939627a3de621c4
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2025-11-30 15:09:39 +0000

    [2. Add Two Numbers] Improve Solution

--- src/main/java/org/mrkrivorotoff/add_two_numbers/Solution.java
+++ src/main/java/org/mrkrivorotoff/add_two_numbers/Solution.java
@@ -4,9 +4,9 @@ import org.mrkrivorotoff.ListNode;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        final var head = new ListNode();
+        final var headNode = new ListNode();
         var carry = 0;
-        for (var currentNode = head; l1 != null || l2 != null || carry != 0; currentNode = currentNode.next) {
+        for (var currentNode = headNode; l1 != null || l2 != null || carry != 0; currentNode = currentNode.next) {
             var sum = carry;
             if (l1 != null) {
                 sum += l1.val;
@@ -19,6 +19,6 @@ class Solution {
             carry = sum / 10;
             currentNode.next = new ListNode(sum % 10);
         }
-        return head.next;
+        return headNode.next;
     }
 }
